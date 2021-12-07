d1f2d03c68f983450d50ae6e69282e30f99336c7
Blaz <blazyy@gmail.com>
2021-12-07 11:50:08 +0100

    added state in store, updated router-links; added bootstrap

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,24 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    allVideosIds: [
+      "uXjLfSFkqTs",
+      "6oo323AQ0JI",
+      "kft2AkvKhWU",
+      "TNzgUCU5TVs",
+      "HHs1ftjayUw",
+      "pbImvaiXGFU",
+      "LrNkecdU6mI",
+      "VXKBaVGWSk0",
+      "MHpHPj-DosY",
+      "NM71cqoNt3w",
+      "DPBXxrhJIv4",
+      "9wKivv-vvhU",
+    ],
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+
+  mutations: {},
+  actions: {},
+  modules: {},
+});
