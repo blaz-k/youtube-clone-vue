5cea30384c78f24c854150049627cf01e13a5768
Blaz <blazyy@gmail.com>
2021-12-22 16:34:35 +0100

    global.css added; media added(mobile first) - sidebar videos css

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import "bootstrap";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./assets/css/global.css";
 
 const app = createApp(App);
 app.use(store);
